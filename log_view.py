0b433af97714bb7db32469c6e38ac4a13841e0ae
Brian Amin <ba555@drexel.edu>
2019-12-03 15:34:46 -0500

    Getting signal statistics

--- log_view.py
+++ log_view.py
@@ -1,6 +1,7 @@
 # python 2.7
 from matplotlib import pyplot as plt
 import scipy.fftpack
+from scipy import signal
 import numpy
 import tkinter as tk
 from tkinter import filedialog
@@ -20,8 +21,17 @@ def fix_timestamp(datetime_str):
     h, m, s = timestamp_str.split(':')
     return float(h) * 3600.0 + float(m) * 60.0 + float(s)
 
+# Remove Bias
+def remove_bias(data, dc_bias):
+    data = [item - dc_bias for item in data]
+    return data
+
+
 # Plot FFT of given set of data
-def plot_fft(data, timestamps, title_str):
+def plot_fft(data, timestamps, title_str, dc_bias):
+    # Remove DC bias from the data (detrend), remove mean
+    data = numpy.asarray(data, dtype=numpy.float32)
+    data = remove_bias(data, dc_bias)
     # Number of samples
     N = len(data)
     Fs = 5.0 # Hz, 5 samples per second
@@ -34,9 +44,9 @@ def plot_fft(data, timestamps, title_str):
     # Plot
     fig, ax = plt.subplots()
     ax.plot(xf, 2.0/N * numpy.abs(data_f[:N//2]))
-    ax.set_title(title_str)
+    ax.set_title("FFT: " + title_str)
     
-    
+ 
 
 
 root = tk.Tk()
@@ -76,6 +86,9 @@ fig3, ax3 = plt.subplots()
 ax3.plot(time_stamps, wind_speed)
 ax3.set_title("Wind Speed")
 
+
+
+
 # Plotting FFTs of wind speed data
 
 # Select region where we're in trim (hover)
@@ -84,15 +97,58 @@ ax4.plot(range(len(wind_speed)), wind_speed)
 ax4.set_title("Wind Speed with Indices")
 # Two sets of trim data
 t1 = 0
-t2 = 200
-t3 = 5500
-t4 = 6500
+t2 = 950
 
 trim_set_1 = wind_speed[t1:t2]
-trim_set2 = wind_speed[t3:t4]
+trim_set_1 = numpy.asarray(trim_set_1, dtype=numpy.float32)
+print("DC Bias Mean: %f" % numpy.mean(trim_set_1))
+dc_bias = numpy.mean(trim_set_1)
 
-plot_fft(trim_set_1, time_stamps[t1:t2], "Wind Speed Trim Set 1 FFT")
-plot_fft(trim_set2, time_stamps[t3:t4], "Wind Speed Trim Set 2 FFT")
-plot_fft(wind_speed, time_stamps, "Wind Speed FFT")
+plot_fft(wind_speed, time_stamps, "Wind Speed", dc_bias)
 
+
+# Get the ground speed data to compare with
+file_path = filedialog.askopenfilename()
+print("SSS Log Path: " + file_path)
+"""
+    Logs will have the following format:
+        Time,SSS_GPS_LAT,SSS_GPS_LON,SSS_GPS_ALT,SSS_GPS_VEL_X,SSS_GPS_VEL_Y,SSS_GPS_HEADING,SSS_GPS_GROUNDSPEED,VEHICLE_LAT,VEHICLE_LON,VEHICLE_ALT,
+        VEHICLE_ALT_DESIRED,VEHICLE_THROTTLE,VEHICLE_GROUNDSPEED,VEHICLE_DISTANCE_ERROR,
+        MOTIONCONTROLLER_SPRING_LENGTH,MOTIONCONTROLLER_TETHER_OUT,
+        MOTIONCONTROLLER_DRUM_SPEED,MOTIONCONTROLLER_DANCER_POSITION,VEHICLE_STATE,MOTIONCONTROLLER_STATE,SSS_GPS_STATE,POWERSUPPLY_STATE,COORDINATOR_STATE,
+        MavLink Msg Rate,MavLink Unique Msg Rate
+"""
+sss_time_stamps = []
+ground_speed = []
+
+with open(file_path) as datafile:
+    csv_reader = csv.reader(datafile, delimiter=',')
+    for row in csv_reader:
+        datetime_str = row[0]
+        sss_time_stamps.append(fix_timestamp(datetime_str))
+        ground_speed.append(row[13])
+
+ground_speed = numpy.asarray(ground_speed, dtype=numpy.float32)
+
+# Plot Detrend data and ground speed to make sure it matches up
+wind_speed_d = numpy.asarray(wind_speed, dtype=numpy.float32)
+wind_speed_d = remove_bias(wind_speed_d, dc_bias)
+fig5, ax5 = plt.subplots()
+ax5.plot(sss_time_stamps, ground_speed, label='Ground Speed')
+ax5.plot(time_stamps, wind_speed_d, label='Debiased Airspeed')
+ax5.set_title("Ground Speed and Debiased Air Speed")
+ax5.legend()
+
+# Calculate Mean Square error between Estimator (Unbiased Airspeed) and the True value (Ground Speed)
+# Smaller the MSE value is, the closer our estimated data is to ground truth
+mse = numpy.square(numpy.subtract(ground_speed, wind_speed_d[0:len(ground_speed)])).mean()
+print("Mean Square Error: %f" % mse)
+
+# Calculate RSME, which is the distance, on average, of a data point from the ground truth line
+rsme = numpy.sqrt(mse)
+print("Root Mean Square Error: %f" % rsme)
+textstr = ("RSME %f" % rsme)
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+ax5.text(0.05, 0.95, textstr, transform=ax5.transAxes, fontsize=11,
+    verticalalignment='top', bbox=props)
 plt.show()
